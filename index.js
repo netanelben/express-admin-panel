a175d34cc7249c54fad6db1d9b8e19b2246b6e11
Nate <mtr574@gmail.com>
2024-02-03 14:39:31 +0200

    Updating dependencies

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express'),
-    bodyparser = require('body-parser'),
-    cookieparser = require('cookie-parser'),
-    session = require('express-session'),
-    passport = require('passport'),
-    MongoDBStore = require('connect-mongodb-session')(session),
-    path = require('path'),
+const express = require("express"),
+    bodyparser = require("body-parser"),
+    cookieparser = require("cookie-parser"),
+    session = require("express-session"),
+    passport = require("passport"),
+    MongoDBStore = require("connect-mongodb-session")(session),
+    path = require("path"),
     PORT = process.env.PORT || 5000,
-    setRoutes = require('./router'),
-    setAuth = require('./router/auth');
+    setRoutes = require("./router"),
+    setAuth = require("./router/auth");
 
 const app = express(),
-    store = new MongoDBStore({ uri: process.env.DB_URL, collection: 'sessions' });
+    store = new MongoDBStore({
+        uri: process.env.DB_URL,
+        collection: "sessions",
+    });
 
-store.on('error', (f) => f);
+store.on("error", (f) => f);
 
-app
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs")
     .use(bodyparser.json())
     .use(bodyparser.urlencoded({ extended: false }))
     .use(cookieparser())
-    .use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }))
+    .use(
+        session({
+            secret: process.env.SESSION_SECRET,
+            resave: true,
+            saveUninitialized: true,
+            cookie: {
+                secure: true,
+                maxAge: 604800000,
+            },
+        })
+    )
     .use(passport.initialize())
-    .use(passport.session())
-    .use(require('express-session')({
-        secret: process.env.SESSION_SECRET,
-        cookie: { maxAge: 604800000 },
-        store, resave: true, saveUninitialized: true
-    }));
+    .use(passport.session());
 
 setAuth(passport);
 setRoutes(app);
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
